f2c3fe9244ca29efde49a19d64fb77ec63214341
Yoan SALLAMI <sallami.yoan@gmail.com>
2016-07-11 16:27:25 +0200

    modified:   src/goal_directed_attention.cpp

--- src/goal_directed_attention.cpp
+++ src/goal_directed_attention.cpp
@@ -840,6 +840,10 @@ private:
       {
         robot_list_.push_back(*(new toaster_msgs::Robot(msg->robotList[i])));
         std::string id = msg->robotList[i].meAgent.meEntity.id;
+        if (id =="pr2")
+        {
+          id="PR2_ROBOT";
+        }
         std::string topicName = "/supervisor/activity_state/"+id;
         if (agent_activity_sub_map_.find(id) == agent_activity_sub_map_.end())
         {
