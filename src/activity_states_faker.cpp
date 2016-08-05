e9c8124d86f5823bda4737629acf8fc967d99cca
Yoan SALLAMI <sallami.yoan@gmail.com>
2016-08-05 16:12:42 +0200

    modified:   src/activity_states_faker.cpp

--- src/activity_states_faker.cpp
+++ src/activity_states_faker.cpp
@@ -22,8 +22,8 @@ public:
 		node_ = node;
 		bool robotActing_=false;
 		action_srv_ = node_.advertiseService("robot_action", &ActivityStatesFaker::Action,this);
-		pr2_pub_ = node_.advertise <supervisor_msgs::AgentActivity> ("supervisor/activity_states/PR2_ROBOT", 1);
-		human_pub_ = node_.advertise <supervisor_msgs::AgentActivity> ("supervisor/activity_states/HERAKLES_HUMAN1", 1);
+		pr2_pub_ = node_.advertise <supervisor_msgs::AgentActivity> ("supervisor/activity_state/PR2_ROBOT", 1);
+		human_pub_ = node_.advertise <supervisor_msgs::AgentActivity> ("supervisor/activity_state/HERAKLES_HUMAN1", 1);
 		timer_ = node_.createTimer(ros::Duration(1.0/30.0), &ActivityStatesFaker::timerCallback, this);
 	}
 private:
