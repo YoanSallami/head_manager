c4ab514ce74adb36c3992bbd4741524adfa763ce
Yoan SALLAMI <sallami.yoan@gmail.com>
2016-08-10 16:41:47 +0200

    new file:   include/optitrack/abort_activity.h 	new file:   include/optitrack/abort_activityRequest.h 	new file:   include/optitrack/abort_activityResponse.h 	new file:   include/optitrack/body_list.h 	new file:   include/optitrack/body_listRequest.h 	new file:   include/optitrack/body_listResponse.h 	new file:   include/optitrack/connectAction.h 	new file:   include/optitrack/connectActionFeedback.h 	new file:   include/optitrack/connectActionGoal.h 	new file:   include/optitrack/connectActionResult.h 	new file:   include/optitrack/connectFeedback.h 	new file:   include/optitrack/connectGoal.h 	new file:   include/optitrack/connectResult.h 	new file:   include/optitrack/connect_port.h 	new file:   include/optitrack/connect_portRequest.h 	new file:   include/optitrack/connect_portResponse.h 	new file:   include/optitrack/connect_service.h 	new file:   include/optitrack/connect_serviceRequest.h 	new file:   include/optitrack/connect_serviceResponse.h 	new file:   include/optitrack/disconnectAction.h 	new file:   include/optitrack/disconnectActionFeedback.h 	new file:   include/optitrack/disconnectActionGoal.h 	new file:   include/optitrack/disconnectActionResult.h 	new file:   include/optitrack/disconnectFeedback.h 	new file:   include/optitrack/disconnectGoal.h 	new file:   include/optitrack/disconnectResult.h 	new file:   include/optitrack/genom_state_activity.h 	new file:   include/optitrack/genom_state_component.h 	new file:   include/optitrack/genom_state_rusage.h 	new file:   include/optitrack/genom_state_stats.h 	new file:   include/optitrack/genom_state_task.h 	new file:   include/optitrack/get_export.h 	new file:   include/optitrack/get_exportRequest.h 	new file:   include/optitrack/get_exportResponse.h 	new file:   include/optitrack/get_local_port.h 	new file:   include/optitrack/get_local_portRequest.h 	new file:   include/optitrack/get_local_portResponse.h 	new file:   include/optitrack/get_logfile.h 	new file:   include/optitrack/get_logfileRequest.h 	new file:   include/optitrack/get_logfileResponse.h 	new file:   include/optitrack/kill.h 	new file:   include/optitrack/killRequest.h 	new file:   include/optitrack/killResponse.h 	new file:   include/optitrack/optitrack_ids_pubdata_s.h 	new file:   include/optitrack/optitrack_marker_pos.h 	new file:   include/optitrack/optitrack_marker_set.h 	new file:   include/optitrack/or_pose_estimator_state.h 	new file:   include/optitrack/or_t3d_acc.h 	new file:   include/optitrack/or_t3d_acc_cov.h 	new file:   include/optitrack/or_t3d_pos.h 	new file:   include/optitrack/or_t3d_pos_cov.h 	new file:   include/optitrack/or_t3d_vel.h 	new file:   include/optitrack/or_t3d_vel_cov.h 	new file:   include/optitrack/or_time_ts.h 	new file:   include/optitrack/pingAction.h 	new file:   include/optitrack/pingActionFeedback.h 	new file:   include/optitrack/pingActionGoal.h 	new file:   include/optitrack/pingActionResult.h 	new file:   include/optitrack/pingFeedback.h 	new file:   include/optitrack/pingGoal.h 	new file:   include/optitrack/pingResult.h 	new file:   include/optitrack/refresh.h 	new file:   include/optitrack/refreshRequest.h 	new file:   include/optitrack/refreshResponse.h 	new file:   include/optitrack/set_export.h 	new file:   include/optitrack/set_exportRequest.h 	new file:   include/optitrack/set_exportResponse.h 	new file:   include/optitrack/set_local_port.h 	new file:   include/optitrack/set_local_portRequest.h 	new file:   include/optitrack/set_local_portResponse.h 	new file:   include/optitrack/set_logfile.h 	new file:   include/optitrack/set_logfileRequest.h 	new file:   include/optitrack/set_logfileResponse.h 	new file:   include/optitrack/unset_logfile.h 	new file:   include/optitrack/unset_logfileRequest.h 	new file:   include/optitrack/unset_logfileResponse.h 	new file:   src/mocap_calib.cpp

---
+++ src/mocap_calib.cpp
@@ -0,0 +1,137 @@
+#include <ros/ros.h>
+#include <string>
+#include <cstdlib>
+#include "../include/head_manager/HeadManagerException.h"
+#include "../include/optitrack/or_pose_estimator_state.h"
+#include "toaster_msgs/ObjectListStamped.h"
+#include "toaster_msgs/Entity.h"
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
+using namespace std;
+
+typedef message_filters::sync_policies::ApproximateTime<optitrack::or_pose_estimator_state, toaster_msgs::ObjectListStamped> MySyncPolicy;
+typedef std::vector < toaster_msgs::Object > ObjectList_t;
+
+class MocapCalib
+{
+private:
+	ros::NodeHandle node_;
+	ros::Subscriber mocap_tag_sub_;
+	ros::Subscriber object_list_sub_;
+	ObjectList_t object_list_;
+	ros::Timer timer_;
+	ros::ServiceServer action_srv_;
+	bool robotActing_;
+	std::string object_;
+public:
+	double mocap_x_;
+	double mocap_y_;
+	double mocap_z_;
+public:
+  MocapCalib(ros::NodeHandle node)
+  {
+  	node_ = node;
+  	mocap_tag_sub_ = node_.subscribe("/optitrack/bodies/MIRE", 1, &MocapCalib::mocapCallback, this);
+  	object_list_sub_ = node_.subscribe("/pdg/objectList", 1, &MocapCalib::objectCallback, this);
+  }
+private:
+  void mocapCallback(const optitrack::or_pose_estimator_state::ConstPtr &mocap)
+  {
+	mocap_x_=mocap->pos[0].x;
+	mocap_y_=mocap->pos[0].y;
+	mocap_z_=mocap->pos[0].z;
+  }
+
+  void objectCallback(const toaster_msgs::ObjectListStamped::ConstPtr &msg)
+  {
+  	try
+    {
+      if(!msg->objectList.empty())
+      {
+        object_list_.clear();
+        for (unsigned int i = 0; i < msg->objectList.size(); ++i)
+        {
+          if (msg->objectList[i].meEntity.id!="unknown object")
+          {
+            object_list_.push_back(*(new toaster_msgs::Object(msg->objectList[i])));
+          }
+        }
+      }
+    }
+    catch (HeadManagerException& e )
+    {
+      ROS_ERROR("[mocap_calib] Exception was caught : %s",e.description().c_str());
+    }
+  }
+public:
+   /****************************************************
+   * @brief : Get object entity from object list
+   * @param : object's id
+   * @return : object entity
+   ****************************************************/
+  toaster_msgs::Entity getObject(std::string id)
+  {
+    double x,y,z;
+    std::vector<float> offset;
+    if(!object_list_.empty())
+    {
+      for (unsigned int i = 0; i < object_list_.size(); ++i)
+      {
+        if (object_list_[i].meEntity.id == id)
+        {
+          std::string offset_str = "offset_"+object_list_[i].meEntity.id;
+          if (node_.hasParam(offset_str))
+          {
+            offset.clear();
+            if (node_.getParam((const string) offset_str,offset))
+            {
+              object_list_[i].meEntity.pose.position.x+=offset[0];
+              object_list_[i].meEntity.pose.position.y+=offset[1];
+              object_list_[i].meEntity.pose.position.z+=offset[2];
+            }
+          }
+          return (object_list_[i].meEntity);
+        }
+      }
+    } else {
+      throw HeadManagerException ( "Could not get object entity :\""+id+"\" in an empty object list." );
+    }
+  }
+
+
+};
+/****************************************************
+ * @brief : Main process function
+ * @param : arguments count
+ * @param : arguments values
+ ****************************************************/
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "activity_states_faker");
+  ros::NodeHandle n;
+  MocapCalib * cm = new MocapCalib(n);
+  ROS_INFO("[mocap_calib] Make sure that ar_track_alvar and pdg is running !");
+  ROS_INFO("[mocap_calib] Make sure that ar_track_alvar detection is enable and pdg stream managed !");
+  ROS_INFO("[mocap_calib] Make sure that the calibration tag is visible by the robot and the mocap system !");
+  while(ros::ok())
+  {
+  	ros::spinOnce();
+  	try
+  	{
+  		toaster_msgs::Entity chess=cm->getObject("CHESSBOARD");
+  		double pos_x=cm->mocap_x_-chess.pose.position.x;
+  		double pos_y=cm->mocap_y_-chess.pose.position.y;
+  		double pos_z=cm->mocap_z_-chess.pose.position.z;
+  		ros::param::set("mocap_calib_world_x",pos_x);
+  		ros::param::set("mocap_calib_world_y",pos_y);
+  		ros::param::set("mocap_calib_world_z",pos_z);
+  		ROS_INFO("[mocap_calib] Position offset = [x:%f, y:%f, z: %f]",pos_x,pos_y,pos_z);
+  	}
+  	catch (HeadManagerException& e )
+    {
+      ROS_ERROR("[mocap_calib] Exception was caught : %s",e.description().c_str());
+    }
+  }
+}
