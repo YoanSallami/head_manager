cce346926d0988aa05a7d03ed2dac0f5ec31fb9e
Yoan Sallami <ysallami@aoide.laas.fr>
2016-11-09 14:37:29 +0100

    modified:   stack.sh

--- stack.sh
+++ stack.sh
@@ -1,12 +1,3 @@
-rosservice call /pdg/manage_stream "{morseHuman: false, niutHuman: false, groupHuman: false, mocapHuman: false, adreamMocapHuman: true, toasterSimuHuman: false, pr2Robot: true, spencerRobot: false, toasterSimuRobot: false, toasterSimuObject: false, arObject : true}" 
-
-rosservice call /database_manager/execute "{command: 'EMPTY', type: 'ALL', facts: [], agent: '', order: '', areaTopic: false ,agentTopic: false ,move3dTopic: false ,pdgTopic: false}"
-
-rosservice call /agent_monitor/monitor_all_agents "monitorAll: true"
-rosservice call /agent_monitor/add_joint_to_agent "{jointName: 'head', agentId: 'HERAKLES_HUMAN1'}"
-rosservice call /agent_monitor/add_joint_to_agent "{jointName: 'rightHand', agentId: 'HERAKLES_HUMAN1'}"
-rosservice call /agent_monitor/add_joint_to_agent "{jointName: 'laser_tilt_mount_link', agentId: 'pr2'}"
-
 rosservice call /area_manager/add_area "myArea:
   id: 0
   name: 'interaction'
@@ -38,17 +29,8 @@ rosservice call /area_manager/add_area "myArea:
   ray: 1.0
   poly:
     points:
-    - {x: 0.5, y: -1, z: 0}
-    - {x: 0.5, y: 1, z: 0}
-    - {x: -0.5, y: 1, z: 0}
-    - {x: -0.5, y: -1, z: 0}
+    - {x: 0.4, y: -0.9, z: 0}
+    - {x: 0.4, y: 0.9, z: 0}
+    - {x: -0.4, y: 0.9, z: 0}
+    - {x: -0.4, y: -0.9, z: 0}
   insideEntities: [0]"
-
-  rosservice call /database_manager/set_info "{add: true, infoType: 'FACT', agentId: 'PR2_ROBOT', facts: [
-    {property: 'type', propertyType: 'state', subProperty: '', subjectId: RED_CUBE'', targetId: 'CUBE', subjectOwnerId: '', targetOwnerId: '', valueType: false, factObservability: 0.0, doubleValue: 0.0, stringValue: '', confidence: 0.0, time: 0, timeStart: 0, timeEnd: 0}
-    , {property: 'type', propertyType: 'state', subProperty: '', subjectId: 'GREEN_CUBE', targetId: 'CUBE', subjectOwnerId: '', targetOwnerId: '', valueType: false, factObservability: 0.0, doubleValue: 0.0, stringValue: '', confidence: 0.0, time: 0, timeStart: 0, timeEnd: 0}
-    , {property: 'type', propertyType: 'state', subProperty: '', subjectId: 'BLACK_CUBE', targetId: 'CUBE', subjectOwnerId: '', targetOwnerId: '', valueType: false, factObservability: 0.0, doubleValue: 0.0, stringValue: '', confidence: 0.0, time: 0, timeStart: 0, timeEnd: 0}
-    , {property: 'type', propertyType: 'state', subProperty: '', subjectId: 'BLUE_CUBE', targetId: 'CUBE', subjectOwnerId: '', targetOwnerId: '', valueType: false, factObservability: 0.0, doubleValue: 0.0, stringValue: '', confidence: 0.0, time: 0, timeStart: 0, timeEnd: 0}
-    , {property: 'type', propertyType: 'state', subProperty: '', subjectId: 'TABLE_4', targetId: 'CUBE', subjectOwnerId: '', targetOwnerId: '', valueType: false, factObservability: 0.0, doubleValue: 0.0, stringValue: '', confidence: 0.0, time: 0, timeStart: 0, timeEnd: 0}
-    , {property: 'type', propertyType: 'state', subProperty: '', subjectId: 'PLACEMAT_RED', targetId: 'AREA', subjectOwnerId: '', targetOwnerId: '', valueType: false, factObservability: 0.0, doubleValue: 0.0, stringValue: '', confidence: 0.0, time: 0, timeStart: 0, timeEnd: 0}
-], event: {property: '', propertyType: '', subProperty: '', subjectId: '', targetId: '', subjectOwnerId: '', targetOwnerId: '', valueType: false, factObservability: 0.0, doubleValue: 0.0, stringValue: '', confidence: 0.0, time: 0}, id: '', name: '', ownerId: ''}"
