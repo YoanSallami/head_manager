30399eba96b22ba36c935a9d2891890877390a52
Yoan Sallami <ysallami@aoide.laas.fr>
2016-11-23 16:37:12 +0100

    modified:   src/robot_observer_3.5.cpp

--- src/robot_observer_3.5.cpp
+++ src/robot_observer_3.5.cpp
@@ -188,7 +188,7 @@ struct ObserverStateMachine_ : public msm::front::state_machine_def<ObserverStat
        //  +----------------------+-----------------+--------------------------+----------------------------+------------------------------------+
      a_row < LookingAction        , humanNotNear        , Waiting              , &sm::rest                                                       >,
     a_irow < LookingAction        , humanHandOnTable                           , &sm::stay_focus_action                                          >,
-       row < LookingAction        , humanHandMove       , LookingNextAction    , &sm::focus_next_action                                          >,
+       row < LookingAction        , humanHandMove       , LookingNextAction    , &sm::focus_next_action      ,&sm::enable_next_action            >,
       //  +-----------------------+---------------------+-----------------------+----------------------------+------------------------------------+
      a_row < LookingNextAction    , humanNotNear        , Waiting              , &sm::rest                                                       >,
      a_row < LookingNextAction    , humanActing         , LookingAction        , &sm::focus_action                                               >,
